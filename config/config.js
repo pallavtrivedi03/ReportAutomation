f3c98db7fb9e573657135b099eeac479d47d46d6
Pallav Trivedi <pallavtrivedi03@gmail.com>
2019-03-03 17:50:25 +0530

    Create user and login done.

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongo_user: "admin",
+    mongo_password: "admin",
+    mongo_database: "ReportAutomation",
+    mongo_host: "localhost",
+    mongo_port: "27017"
+}
---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: false
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
